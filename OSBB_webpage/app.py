230f68aedf2a61a33542c0f43a63f036f00e8b18
Yurii <yurasidor26@gmail.com>
2022-07-18 11:00:14 +0300

    sending qa form

--- OSBB_webpage/app.py
+++ OSBB_webpage/app.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import pyodbc as pdb
 
 app = Flask(__name__)
 
 
-def info():
+def db_connection():
     db_conection = pdb.connect('Driver={SQL Server};'
                                'Server=PC-HOME;'
-                               'Database=debtors;'
+                               'Database=osbb_info;'
                                'Trusted_Connection=yes;')
-    cursor = db_conection.cursor()
+    return db_conection
+
+
+def debtors_info():
+    cursor = db_connection().cursor()
     cursor.execute("select * from debtor_inf order by debt desc")
     return cursor.fetchall()
 
 
+def qa_message(surname, name, flat_num, email, message):
+    cnxn = db_connection()
+    cursor = cnxn.cursor()
+    cursor.execute(f"insert into messages values ('{surname}', '{name}', {flat_num}, '{email}', '{message}')")
+    cnxn.commit()
+
+
 @app.route('/', methods=['POST', 'GET'])
 def main():
-    return render_template('index.html', info=info())
+    return render_template('index.html', info=debtors_info())
 
 
 @app.route('/qa', methods=['POST', 'GET'])
@@ -28,12 +39,13 @@ def QA():
         flat_num = request.form.get('flat_num')
         email = request.form.get('email')
         message = request.form.get('message')
-        print(not email)
         if not surname or not name or not flat_num or not email or not message or not request.form.getlist(
                 'agree'):
-             return render_template('qa_error.html', surname=surname, name=name, flat_num=flat_num, email=email,
-                            message=message)
-
+            return render_template('qa_error.html', surname=surname, name=name, flat_num=flat_num, email=email,
+                                   message=message)
+        else:
+            qa_message(surname, name, flat_num, email, message)
+            return redirect(url_for('main'))
     return render_template('qa_form.html', check=check)
 
 
